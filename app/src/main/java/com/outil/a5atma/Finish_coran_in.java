fbb2ea5a2325d3aef8598430c8bcd45d67f16c29
unknown <bendioussoussama@gmail.com>
2021-04-23 16:31:46 +0200

    principale

---
+++ app/src/main/java/com/outil/a5atma/Finish_coran_in.java
@@ -0,0 +1,14 @@
+package com.outil.a5atma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Finish_coran_in extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finish_coran_in);
+    }
+}
---
+++ app/src/main/java/com/outil/a5atma/Principale.java
@@ -0,0 +1,18 @@
+package com.outil.a5atma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Principale extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principale);
+    }
+
+    public void gotoactivity(View view) {
+    }
+}
