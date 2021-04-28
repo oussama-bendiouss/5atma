e05533604e2996bd89993afb536bbda8cc75237c
unknown <bendioussoussama@gmail.com>
2021-04-28 15:51:17 +0200

    msg for how you can finish the coran

--- app/src/main/java/com/outil/a5atma/How_to_finish.java
+++ app/src/main/java/com/outil/a5atma/How_to_finish.java
@@ -8,7 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 
 public class How_to_finish extends AppCompatActivity {
-
+   String msg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,11 +18,28 @@ public class How_to_finish extends AppCompatActivity {
     public void how_many(View view) {
         String tag = view.getTag().toString();
 
+        switch(tag){
+            case "1":
+                msg = "لختم القرآن مرة واحدة في الشهر،يجب عليك قراءة حزب بعد صلاة الفجر و أخر بعد صلاة المغرب";
+                break;
+            case "2":
+                msg = "لختم القرآن مرتين في الشهر، يجب عليك قراءة حزب بعد صلاة الفجر،الضهر،المغرب،العشاء. لماذا هذه الأوقات بالضبط؟ لأنكأخي المسلم في هذه الأوقات تكون غير مشغول بأمور الدنيا";
+                break;
+            case "3":
+                msg ="لختم القرأن ثلات مرات ، يجب عليك قراءة حزب قبل صلاة الفجر و المغرب ، و حزب بعد صلاة الفجر و المغرب، بالإضافة إلى حزب بعد صلاة الضهر و العشاء";
+                break;
+            case "4":
+                msg="لختم القرأن أربع مرات عليك بقراءة حزب بعد و قبل كل صلاة ما عد صلاة العصر";
+                break;
+            case "5":
+                msg="لختم القرأن خمس مرات في الشهر عليك أخي المسلم بقراءة حزب قبل و  بعد كل صلاة، وبهذا تكون قرأت في اليوم الواحد عشرة أحزاب .";
+                break;
+        }
         AlertDialog.Builder builder = new AlertDialog.Builder(How_to_finish.this);
-        builder.setMessage(tag)
+        builder.setMessage(msg)
                 .setCancelable(true)
-                .setTitle(tag)
-                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                .setTitle("إختم القرآن الكريم "+tag+" مرة ")
+                .setNegativeButton("خروج", new DialogInterface.OnClickListener() {
                     /**
                      * This method will be invoked when a button in the dialog is clicked.
                      *
