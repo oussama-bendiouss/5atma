b63ccc76d428f2eabfcea136055d0422cecf7e04
unknown <bendioussoussama@gmail.com>
2021-05-15 12:33:17 +0200

    ajouter un coran dans l'application

--- app/src/main/java/com/outil/a5atma/Coran.java
+++ app/src/main/java/com/outil/a5atma/Coran.java
@@ -11,14 +11,28 @@ import com.shockwave.pdfium.PdfDocument;
 
 public class Coran extends AppCompatActivity {
 
-
+    PDFView pdf;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coran);
-
-
         setContentView(R.layout.activity_coran);
+         pdf = findViewById(R.id.pdfView);
+
+        pdf.fromAsset("coran.pdf")
+                .enableSwipe(true) // allows to block changing pages using swipe
+                .swipeHorizontal(false)
+                .enableDoubletap(true)
+                .defaultPage(0)
+                .enableAnnotationRendering(false) // render annotations (such as comments, colors or forms)
+                .password(null)
+                .scrollHandle(null)
+                .enableAntialiasing(true) // improve rendering a little bit on low-res screens
+                // spacing between pages in dp. To define spacing color, set view background
+                .spacing(0)
+                .pageFitPolicy(FitPolicy.WIDTH) // mode to fit pages in the view
+                .load();
+        
 
 
     }
